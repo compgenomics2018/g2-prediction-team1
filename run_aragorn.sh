dbc34925f4189918254a616ef4c094eedb4c1a22
Caban Figueroa, Victoria Maria <vcaban3@gatech.edu>
2018-03-25 20:22:11 -0400

    Update run_aragorn.sh
    Added information as comments about useful flags

--- run_aragorn.sh
+++ run_aragorn.sh
@@ -3,7 +3,12 @@
 #For running this simple script using Aragorn, the aragorn script itself must be in the same directory as this script
 #This script goes through all the folders in a directory and runs aragorn -t and outputs results to previously created directory
 #check for the location of the input folders as well as for the output, these are currently hardcoded
-# t =>  searches for tRNA genes 
+# 
+# -t =>  searches for tRNA genes
+# -o => indicates output file (not used in this script)
+# -fasta => Print out primary sequence in fasta format
+# -fo => Prints out primary sequence in fasta format only (no secondary structure)
+
 
 for file in /projects/data/team1_GenePrediction/true_assemblies/*; 
 	do 
