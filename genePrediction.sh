85153e258932a4f3a004a05f29aa19a1a5a33ff7
Smith, Casey <clsmith@gatech.edu>
2018-03-26 19:07:07 -0400

    added info for GeneMarkS

--- genePrediction.sh
+++ genePrediction.sh
@@ -39,20 +39,29 @@ DIR=$(pwd); #this captures the main working directory as the variable DIR
 #we will assume that a person running this script will be in their home directory on the server and have the assembly file(s) in that same directory
 
 #run Prodigal
-prodigal -i $iopt -f gff -o "$oopt/prodigal_output.gff" -d "$oopt/prodigal_nucleotide.fa" -a "$oopt/prodigal_protein.fa" 
+#prodigal -i $iopt -f gff -o "$oopt/prodigal_output.gff" -d "$oopt/prodigal_nucleotide.fa" -a "$oopt/prodigal_protein.fa" 
 
-#run GeneMarkHMM
-#perl "/projects/data/team1_GenePrediction/bin/genemark_suite_linux_64/gmsuite/gmhmmp.pl" --output genemark_output.GFF --format GFF $iopt  #not working yet
+#run GeneMarkS
+cd /projects/data/team1_GenePrediction/bin/genemark_suite_linux_64/gmsuite/
+perl gmsn.pl --prok --output /projects/home/csmith366/outputs/genemarkS_output.gff --format GFF /projects/home/csmith366/SRR3982229.sspace.final.scaffolds.fasta
+cd ..
 
 #run Aragorn
 #for running aragorn, the aragorn script itself must be in the same directory as this script
 cd /projects/data/team1_GenePrediction/bin/aragorn1.2.38/ #we change into the directory containing aragorn
-./aragorn -t "$DIR/$iopt" -o "$DIR/$oopt/aragorn_output"
+./aragorn -t "$DIR/$iopt" -fo -o "$DIR/$oopt/aragorn_output"
 cd ..
 
 #run Infernal
 
 #run RNAmmer
+
 #validate outputs
     #to do this, just run validation.sh and have the prodigal output and genemark output in the same directory as well
+cd $oopt
+bash "/projects/data/team1_GenePrediction/validation/validation.sh"
+
+#here we need to delete the extra files (prodigal files, genemark files, and any temporary files that we don't need)
+#the only output should be the merged file from the validation script and the ncRNA output files
+
 exit;
