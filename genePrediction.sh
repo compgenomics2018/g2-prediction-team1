66f1f6d6ed898110225eca04825cc5116d5e121c
Smith, Casey <clsmith@gatech.edu>
2018-03-26 17:51:02 -0400

    edits to run prodigal and aragorn

--- genePrediction.sh
+++ genePrediction.sh
@@ -3,7 +3,7 @@
 #this is the usage message that displays when no input is passed or when -h is passed
 usage="Gene Prediction Pipeline. Command line options:	
 -i Input assembly file
--o Output file 
+-o Output directory 
 -h usage information "
 
 if [ $# == 0 ] ; then	#if nothing is input, then usage message is printed and the script exits
@@ -27,38 +27,32 @@ else
 	echo "$iopt: no such file exists"; exit;
 fi
 
-if [ -d outputs ]
+if [ -d $oopt ] #this checks if the output directory already exists
 then
     :
 else
-    mkdir outputs
+    mkdir $oopt
+fi
 
-cd outputs
-if [ -e $oopt ]	#this checks if the output file already exists
-then
-	echo "$oopt already exists. would you like to overwrite the file or exit the program? (type overwrite or exit) "
-	read response
-        if [ "$response" == "overwrite" ]
-        then
-	        rm $oopt #removes existing output file (in output directory) so we can make a new file
-        else
-	        exit;
-fi
-fi
+DIR=$(pwd); #this captures the main working directory as the variable DIR
 
 #we will assume that a person running this script will be in their home directory on the server and have the assembly file(s) in that same directory
 
 #run Prodigal
-Prodigal -i "$iopt" -f gff -o prodigal_output.gff -d prodigal_nucleotide.fa -a prodigal_protein.fa 
+prodigal -i $iopt -f gff -o "$oopt/prodigal_output.gff" -d "$oopt/prodigal_nucleotide.fa" -a "$oopt/prodigal_protein.fa" 
 
 #run GeneMarkHMM
-perl gmhmmp.pl --output genemark_output.GFF --format GFF "$iopt"
+#perl "/projects/data/team1_GenePrediction/bin/genemark_suite_linux_64/gmsuite/gmhmmp.pl" --output genemark_output.GFF --format GFF $iopt  #not working yet
 
 #run Aragorn
-./aragorn -t "$iopt" -o outputs/aragorn_output #must make this a directory first
+#for running aragorn, the aragorn script itself must be in the same directory as this script
+cd /projects/data/team1_GenePrediction/bin/aragorn1.2.38/ #we change into the directory containing aragorn
+./aragorn -t "$DIR/$iopt" -o "$DIR/$oopt/aragorn_output"
+cd ..
 
 #run Infernal
+
 #run RNAmmer
 #validate outputs
-
-
+    #to do this, just run validation.sh and have the prodigal output and genemark output in the same directory as well
+exit;
