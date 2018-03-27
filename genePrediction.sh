d242b2416464251450267c537152dee8190c1b13
Smith, Casey <clsmith@gatech.edu>
2018-03-27 11:53:16 -0400

    Validation commands working now

--- genePrediction.sh
+++ genePrediction.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-export PATH=/projects/data/team1_GenePrediction/bin/Prodigal/:/projects/data/team1_GenePrediction/bin/rnammer1.2:/projects/data/team1_GenePrediction/bin/infernal-1.1.2/src/:$PATH
+export PATH=/projects/data/team1_GenePrediction/bin/Prodigal/:/projects/data/team1_GenePrediction/bin/Prodigal/rnammer1.2:$PATH
 
 #this is the usage message that displays when no input is passed or when -h is passed
 usage="Gene Prediction Pipeline. Command line options:	
@@ -38,13 +38,10 @@ else
 fi
 
 DIR=$(pwd); #this captures the main working directory as the variable DIR
-SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-cp .gm_key ~/
-
-#we will assume that a person running this script will be in their home directory on the server and have the assembly file(s) in that same directory
 
 #run Prodigal
-prodigal -i $iopt -f gff -o "$oopt/prodigal_output.gff" -d "$oopt/prodigal_nucleotide.fa" -a "$oopt/prodigal_protein.fa" 
+prodigal -i $iopt -f gff -o "$oopt/prodigal_output.gff"
+#flags we no longer need: -d "$oopt/prodigal_nucleotide.fa" -a "$oopt/prodigal_protein.fa" 
 
 #run GeneMarkS
 cd /projects/data/team1_GenePrediction/bin/genemark_suite_linux_64/gmsuite/
@@ -61,31 +58,29 @@ cd /projects/data/team1_GenePrediction/bin/aragorn1.2.38/ #we change into the di
 cd ..
 
 #run Infernal
-cmscan --tblout "$oopt/`basename $iopt`.RF01400.cm.tblout" --fmt 2 "/projects/data/team1_GenePrediction/bin/infernal-1.1.2/cm/RF01400.cm" $iopt > trash.cmscan
-`rm trash.cmscan`
+#bash run_infernal.sh $iopt $oopt 
 
 #run RNAmmer
-echo $iopt > file_list_RNAmmer
-perl $SCRIPT_DIR/run_rnammer.pl -i file_list_RNAmmer -d `dirname $iopt`
-rm file_list_RNAmmer
-mv *fsa* $oopt
+#echo $iopt > file_list_RNAmmer
+#perl run_rnammer.pl -i file_list_RNAmmer -d `dirname $iopt`
+#rm file_list_RNAmmer
+#mv *fsa* $oopt
 
 #validate outputs
 #to do this, just run validation.sh and have the prodigal output and genemark output in the same directory as well
-mv prodigal_output.gff /projects/data/team1_GenePrediction/validation/
-mv genemarkS_output.gff /projects/data/team1_GenePrediction/validation/
-cd /projects/data/team1_GenePrediction/validation/
+cd $oopt
+mv prodigal_output.gff "/projects/data/team1_GenePrediction/validation/"
+mv genemarkS_output.gff "/projects/data/team1_GenePrediction/validation/"
+cd "/projects/data/team1_GenePrediction/validation/"
 bash validation_wrapper.sh
-bash union_wrapper.sh > $oopt
+bash union_wrapper.sh > "$DIR/$oopt/protein_coding_result.gff"
 rm prodigal_output.gff
 rm genemarkS_output.gff
-cd ..
+cd $DIR
  
 #we need to add the gff to fasta file (but we need to keep both the .gff and the .fa files)
-perl "/projects/data/team1_GenePrediction/bin/genemark_suite_linux_64/gmsuite/GFF2fasta.pl" -i union.gff -a $iopt -o ab_initio_final_result.fasta
+#perl "/projects/data/team1_GenePrediction/bin/genemark_suite_linux_64/gmsuite/GFF2fasta.pl" -i union.gff -a $iopt -o protein_coding_result.fasta
 #here we need to delete the extra files (prodigal files, genemark files, and any temporary files that we don't need)
-rm sequence 
-rm itr_0.mod
-#the only output should be the merged file from the validation script (ab_initio_final_result.fasta) and the ncRNA output files (gff and fasta form)
+#the only output should be the merged file from the validation script (protein_coding_result.fasta) and the ncRNA output files (gff and fasta form)
 
 exit;
