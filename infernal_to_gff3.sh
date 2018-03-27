3e8722e33a4dd506b08b2da248aeaeefcfe4cbca
Liu, Yiqiuyi <liuyiqiuyi@gatech.edu>
2018-03-27 09:15:44 -0400

    Update infernal_to_gff3.sh

--- infernal_to_gff3.sh
+++ infernal_to_gff3.sh
@@ -9,10 +9,6 @@ mkdir gff3_output;
     
 for f in *.tblout; do
 	echo "##gff-version 3" > gff3_output/$f.gff;
-	awk '{if (substr($0, 0, 1) != "#") printf "%s\t%s\t%s\t%d\t%d\t%d\t%s\t%s\ttarget name=%s, accession=%s\n",$4,"Infernal-1.1.2","sRNA",$10,$11,$18,$12,".",$2,$3}' $f >> gff3_output/$f.gff;
+	awk '{if (substr($0, 0, 1) != "#") printf "%s\t%s\t%s\t%d\t%d\t%.5f\t%s\t%s\ttarget name=%s, accession=%s\n",$4,"Infernal-1.1.2","sRNA",$10,$11,$18,$12,".",$2,$3}' $f >> gff3_output/$f.gff;
 
 done
-
-
-
-
