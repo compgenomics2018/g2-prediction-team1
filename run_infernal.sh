c62e9ebe95160d0cd7f70d496d61d0c947afd30a
Wist, Stephen Mandis <swist@gatech.edu>
2018-03-26 20:23:18 -0400

    added script to run infernal

--- run_infernal.sh
+++ run_infernal.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
-export PATH=/projects/data/team1_GenePrediction/bin/infernal-1.1.2/src/:$PATH
 
-file=$1
-outdir=$2
+# this runs infernal's `cmscan` program 
+# before using this, add to your path PATH=$PATH:/projects/data/team1_GenePrediction/bin/infernal-1.1.2/src
+# --tblout and --fmt write output in a more concise format
+# standard cmscan output is all written to trash.cmscan because it is less easily parsed than the
+# tblout format and therefore not desirable 
 
-cmscan --tblout "$outdir/`basename $file`.RF01400.cm.tblout" --fmt 2 "/projects/data/team1_GenePrediction/bin/infernal-1.1.2/cm/RF01400.cm" $file > trash.cmscan
+cd /projects/data/team1_GenePrediction/true_assemblies
+for f in *.fasta; do
+
+	for cm in  RF01400.cm; do
+		sh -c "cmscan --tblout /projects/data/team1_GenePrediction/infernal_output/$f.$cm.tblout --fmt 2 /projects/data/team1_GenePrediction/bin/infernal-1.1.2/cm/$cm $f > trash.cmscan"
+	done
+done
