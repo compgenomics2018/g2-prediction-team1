bba5ce7244ff26170d1e5132afc9f0cee776022a
Ou, Yihao <ouyihao@gatech.edu>
2018-04-05 22:18:06 -0400

    updated bedtools command line

--- validation.sh
+++ validation.sh
@@ -20,7 +20,7 @@ intersect () {
 	
 	for ((j=0;j<=$(($i-1));j+=1)) ##iterate through the array to get the file name from file name list
 	do
-	bedtools intersect -f 0.99 -r -a ${array1[$j]} -b ${array2[$j]}> "intersect_${array3[$j]}.gff"  ##find intersection of the results between 2 tools with 99% coverage using bedtools  	
+	bedtools intersect -f 0.80 -r -s -wa -a ${array1[$j]} -b ${array2[$j]}> "intersect_${array3[$j]}.gff"  ##find intersection of the results between 2 tools with 99% coverage using bedtools  	
 	done
 }
 
@@ -43,7 +43,7 @@ coverage () {  ##calculate the coverage of each tool in order to find the best t
 	
 	for ((j=0;j<=$(($i-1));j+=1))
 	do
-	bedtools intersect -f 0.99 -r -a ${array1[$j]} -b ${array2[$j]} > "overlap_${array1[$j]}_${array2[$j]}"
+	bedtools intersect -f 0.80 -r -s -a ${array1[$j]} -b ${array2[$j]} > "overlap_${array1[$j]}_${array2[$j]}"
 	matching=$(cat "overlap_${array1[$j]}_${array2[$j]}" | wc -l) ##count for the number of matching gene between the result of a tool and the merged result
 	total_gene=$(cat ${array2[$j]} | wc -l) ##count the number of total genes after merging
 	coverage=$(echo $(((matching * 100) / total_gene)) | bc) ##calculate the coverage for one genome
