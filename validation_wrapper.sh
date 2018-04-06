376a0b561923b886dab4357ddde0b57646e9c389
Ou, Yihao <ouyihao@gatech.edu>
2018-04-06 13:47:20 -0400

    update bedtools command

--- validation_wrapper.sh
+++ validation_wrapper.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/bash
 
 intersect () {
-	bedtools intersect -f 0.99 -r -a $1 -b $2 > "intersect.gff"  ##find intersection of the results between 2 tools with 99% coverage using bedtools  	
+	bedtools intersect -f 0.80 -r -s -a $1 -b $2 > "intersect.gff"  ##find intersection of the results between 2 tools with 99% coverage using bedtools  	
 }
 
 intersect "prodigal_output.gff" "genemarkS_output.gff" 
 
 
 coverage () {  ##calculate the coverage of each tool in order to find the best tool
-	bedtools intersect -f 0.99 -r -a $1 -b $2 > "overlap_$1_$2"
+	bedtools intersect -f 0.80 -r -s-a $1 -b $2 > "overlap_$1_$2"
 	matching=$(cat "overlap_$1_$2" | wc -l) ##count for the number of matching gene between the result of a tool and the merged result
 	total_gene=$(cat $2 | wc -l) ##count the number of total genes after merging
 	coverage=$(echo $(((matching * 100) / total_gene)) | bc) ##calculate the coverage for one genome
