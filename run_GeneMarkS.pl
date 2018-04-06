8bf02daf53c10bbe5c7d14299ecb6186143649df
Yue, Qinyu <qyue7@gatech.edu>
2018-04-06 15:45:14 -0400

    Create run_GeneMarkS.pl
    use for wrapper

---
+++ run_GeneMarkS.pl
@@ -0,0 +1,19 @@
+#!/usr/bin/perl -w
+#######################
+#before using:
+#this script should run under the path where the asseblied genomes are.
+# useage: perl run_gmhmmp.perl <input.fasta>
+# <input.fasta> should be the nucleotide sequences in fasta format.
+#######################
+
+use strict;
+
+my $filename = ();
+#input file
+$filename = $ARGV[0];
+#open file
+chomp $filename;
+#run GeneMarkhmm, you can replace the command line in ``
+`gmsn.pl --prok --output $filename.HMM.gff --format GFF --fnn --faa $filename`
+#remove intermediate files
+`rm *.mod gms.log`
