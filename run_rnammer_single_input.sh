15eb0af0f90c08444381010a86ec138f33de83d8
Liu, Yiqiuyi <liuyiqiuyi@gatech.edu>
2018-04-09 17:30:09 -0400

    run rnammer on one sample input

---
+++ run_rnammer_single_input.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/bash
+
+###################################
+# Notice:
+# 1. This script is used to run rnammer on a single sample fasta file
+# 2. This script need to be used within the same directory as the input file
+# 3. The output file will be named input_file_name.rnammer.gff
+###################################
+
+mkdir -p ncRNA_output
+test ! $1 && printf "\nplease specify input fasta file\nExample: $0 sample_assembly.fasta\n\n" && exit 1;
+
+printf "\n[rnammer]running rnammer on $1...\n"
+
+/projects/data/team1_GenePrediction/bin/rnammer1.2/rnammer -S bac -m lsu,ssu,tsu -gff ncRNA_output/$1.rnammer.gff $1
+printf "[rnammer]finished running rnammer on $1\n"
+
+# remove extra format information for merging
+sed -i '$ d' ncRNA_output/$1.rnammer.gff $1
+sed -i -e '1,6d' ncRNA_output/$1.rnammer.gff $1
