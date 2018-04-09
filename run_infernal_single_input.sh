fc093e7c1cb1403eeddda6da712974e564ac6e91
Liu, Yiqiuyi <liuyiqiuyi@gatech.edu>
2018-04-09 17:29:38 -0400

    run infernal on one sample input with established cm model

---
+++ run_infernal_single_input.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+###################################                                             
+# Notice:                                                                       
+# 1. This script is used to run infernal cmscan on a single sample fasta file           
+# 2. This script need to be used within the same directory as the input file
+# 3. cm model RF01400.cm is already available in /projects/data/team1_GenePrediction/bin/infernal-1.1.2/cm/
+# 4. The output file will be named input_file_name.aragorn.gff in ncRNA_output/export PATH=$PATH:/projects/data/team1_GenePrediction/bin/infernal-1.1.2/src
+###################################
+
+mkdir -p ncRNA_output
+
+test ! $1 && printf "\nplease specify input fasta file\nExample: $0 sample_assembly.fasta\n\n" && exit 1;
+
+printf "\n[infernal]running infernal cmscan on $1...\n"
+
+# trash default report and output table format
+cmscan --fmt 2 --tblout ncRNA_output/$1.tblout /projects/data/team1_GenePrediction/bin/infernal-1.1.2/cm/RF01400.cm $1 > trash.cmscan
+rm trash.cmscan
+
+printf "\n[infernal]finish infernal cmscan on $1\n"
+printf "\n[infernal]converting output to gff3...\n"
+
+awk '{if (substr($0, 0, 1) != "#" && $19 == "!" ) printf "%s\t%s\t%s\t%d\t%d\t%.5f\t%s\t%s\ttarget name=%s, accession=%s\n",$4,"Infernal-1.1.2","sRNA",$10,$11,$18,$12,".",$2,$3}' ncRNA_output/$1.tblout  > ncRNA_output/$1.temp
+awk '{ if( $4 > $5) {t=$5; $5=$4; $4=t; print} else {print} }' ncRNA_output/$1.temp > ncRNA_output/$1.infernal.gff
+sed -i 's/ /\t/g' "ncRNA_output/$1.infernal.gff"
+
+rm ncRNA_output/$1.tblout
+rm ncRNA_output/$1.temp
