cd790ed27559915c84340137640e34f14345116d
Liu, Yiqiuyi <liuyiqiuyi@gatech.edu>
2018-04-09 17:28:52 -0400

    run aragorn on one sample input

---
+++ run_aragorn_single_input.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/bash                                                                 
+
+###################################                                             
+# Notice:                                                                       
+# 1. This script is used to run aragorn on a single sample fasta file           
+# 2. This script need to be used within the same directory as the input file    
+# 3. The output file will be named input_file_name.aragorn.gff in ncRNA_output/                  
+
+
+mkdir -p ncRNA_output
+
+test ! $1 && printf "\nplease specify input fasta file\nExample: $0 sample_assembly.fasta\n\n" && exit 1;
+
+printf "\n\n[aragorn]running aragorn on $1...\n"
+
+/projects/data/team1_GenePrediction/bin/aragorn1.2.38/aragorn -t -fasta -O ncRNA_output/$1.aragorn.report $1
+
+printf "\n[aragorn]finished running aragorn on $1\n"
+printf "\n[aragorn]converting output report to gff...\n\n"
+
+python crisis_single_input.py ncRNA_output/$1.aragorn.report
+
+rm ncRNA_output/$1.aragorn.report
+
+
