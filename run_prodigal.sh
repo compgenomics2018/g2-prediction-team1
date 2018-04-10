a8b9c76e57bd6c80990726ef46fc2dd53802155d
Ou, Yihao <ouyihao@gatech.edu>
2018-04-09 20:21:31 -0400

    edited output name and path

--- run_prodigal.sh
+++ run_prodigal.sh
@@ -1,14 +1,10 @@
 #!/bin/bash
 
-mkdir Prodigal_out 2> /dev/null;
+#echo "Running Prodigal on $File" > Prodigal_out/"$File".prodigal.txt;
+Prodigal -i $1 -f gff -o prodigal.gff -d prodigal.fna -a prodigal.faa;
+#echo "Finished $File on `date`" >> Prodigal_out/"$File".prodigal.txt;
 
-File=$1;
-
-echo "Running Prodigal on $File" > Prodigal_out/"$File".prodigal.txt;
-Prodigal -i $File -f gff -o Prodigal_out/"$File".prodigal.gff -d Prodigal_out/"$File".prodigal.fna -a Prodigal_out/"$File".prodigal.faa;
-echo "Finished $File on `date`" >> Prodigal_out/"$File".prodigal.txt;
-
-python reformatFasta.py Prodigal_out/"$File".prodigal.fna;
-python reformatFasta.py Prodigal_out/"$File".prodigal.faa;
+python reformatFasta.py prodigal.fna;
+python reformatFasta.py prodigal.faa;
 
 exit
