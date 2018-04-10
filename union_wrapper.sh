d739009589a587e32a429014e03d73a8cbea91ab
Ou, Yihao <ouyihao@gatech.edu>
2018-04-09 20:00:21 -0400

    Added intersect function

--- union_wrapper.sh
+++ union_wrapper.sh
@@ -1,15 +1,22 @@
 #!/usr/bin/bash
 
+intersect () {
+	bedtools intersect -f 0.80 -r -s -wa -a $1 -b $2 > "intersect.gff"  ##find intersection of the results between 2 tools with 99% coverage using bedtools  	
+}
+
+intersect "prodigal.gff" "genemark.gff" 
+
+
 complement () {  ##calculate the coverage of each tool in order to find the best tool
 	bedtools intersect -f 0.80 -r -wa -s -v -a $1 -b $2 > "$3_complement.gff"
 }
 
-complement "prodigal_output.gff" "intersect.gff" "prodigal"
-complement "genemarkS_output.gff" "intersect.gff" "genemarkS"
+complement "prodigal.gff" "intersect.gff" "prodigal"
+complement "genemark.gff" "intersect.gff" "genemarkS"
 
 union (){
 	cat $1 $2 $3 > "union.gff"
 }
 union "prodigal_complement.gff" "genemarkS_complement.gff" "intersect.gff" 
 rm *complement.gff
-rm intersect*
+rm intersect.gff
